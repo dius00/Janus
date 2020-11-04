4ee1bc93697d178459a8411e114ec2f239c63b7c
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2020-11-04 11:49:23 +0900

    reinitialized w/ firebase:init

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom"
 import App from "./components/App"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-console.log(process.env.REACT_APP_API_KEY);
 ReactDOM.render(
   <React.StrictMode>
     <App />
